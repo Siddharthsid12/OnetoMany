46111ad7bb3251aec9c34dead8490af0e3e436a4
Siddharthsid12 <78201614+Siddharthsid12@users.noreply.github.com>
2021-07-12 10:53:20 +0530

    Code pushed

---
+++ src/main/java/com/cg/jpainitialsetup/domain/Project.java
@@ -0,0 +1,40 @@
+package com.cg.jpainitialsetup.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private int id;
+	private String name;
+	@OneToMany
+	private int duration;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	
+}
